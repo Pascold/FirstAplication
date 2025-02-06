9e6e50d3e26ec339f2a209917d344cf6d9ee5fa1
Pascold <walski.pascal@gmail.com>
2025-02-06 15:17:54 +0100

    User name

--- app/src/main/java/com/example/firstaplication/MainActivity.kt
+++ app/src/main/java/com/example/firstaplication/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Zapraszam na XVisionStudio.com \nUżytkownik: $name!",
+        text = "Zapraszam na XVisionStudio.com \nUser: $name!",
         modifier = modifier
     )
 }
